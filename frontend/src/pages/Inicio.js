79e7500ae9c573c93e5d7d56488c3110721397f4
GenaroOlivaresDaniela <al222111448@gmail.com>
2024-11-30 01:28:19 -0600

    .

--- frontend/src/pages/Inicio.js
+++ frontend/src/pages/Inicio.js
@@ -1,33 +1,14 @@
-import React, { useContext,useState, useEffect  } from 'react';
+import React, { useContext } from 'react';
 import { Grid, Typography, IconButton, Avatar } from '@mui/material';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday'; 
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar'; 
 import ChatIcon from '@mui/icons-material/Chat'; 
 import { Link } from 'react-router-dom'; 
 import { UserContext } from '../context/UserContext';
-import VehiculosCard from './../components/Vehiculos'
 
 const Inicio = () => {
     const { user } = useContext(UserContext);
-    const [cardsData, setCardsData] = useState([]);
-    const id_usuario = user.id;
-    
-    const BaseUrl = 'http://localhost:3001/imagenes/vehiculos/'
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-            const response = await fetch(`http://localhost:3001/api/vehiculos/${id_usuario}`);
-            const data = await response.json();
-           
-            setCardsData(data);
-        } catch (error) {
-            console.log("Error al obtener los datos:", error);
-        }
-       
-        };
-
-        fetchData();
-    });
+  
     return (
         <div>
          
@@ -61,24 +42,13 @@ const Inicio = () => {
                             <ChatIcon sx={{ fontSize: 80, color: 'white' }} />
                         </Avatar>
                     </IconButton>
-                    <Typography variant="h6" sx={{fontSize: '22px', fontWeight: 'bold',color: 'black'}}>MIS CITAS</Typography>
+                    <Typography variant="h6" sx={{fontSize: '22px', fontWeight: 'bold',color: 'black', marginButton:'50px'}}>MIS CITAS</Typography>
                 </Grid>
             </Grid>
 
         
 
-            <Grid container spacing={4} sx={{marginTop: '50px'}}>
-            {cardsData.map((card) => (
-                <Grid item xs={12} sm={8} key={card.id}>
-                    <VehiculosCard 
-                        marca={card.marca} 
-                        modelo={card.modelo} 
-                        image={`${BaseUrl}${card.imagen}`}
-                    />
-                  
-                </Grid>
-            ))}
-        </Grid>
+          
 
      
         </div>
